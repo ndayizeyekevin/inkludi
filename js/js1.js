d1402e62bcc9059e8f23e72fdae3472f32a3ae6f
Kevin_N <88332689+ndayizeyekevin@users.noreply.github.com>
2022-10-27 13:55:36 +0200

    Update js1.js

--- js/js1.js
+++ js/js1.js
@@ -8,6 +8,6 @@ $('.navbar-toggler').click(function() {
 document.querySelector('.navbar-toggler').addEventListener('click', function(){ 
   const iconEl = document.getElementsByTagName('I')[0];
   if(this.getAttribute('aria-expanded')) { 
-    iconEl.classList.toggle('icon-color');
+//     iconEl.classList.toggle('icon-color');
   };
 });
